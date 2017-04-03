5e92f6f67d8166504c6e5bce273fab3cc87d93f8
Plugin4UGit <plugin4u@outlook.com>
2017-04-03 09:07:51 +0300

    1.0.0.1

--- ForceDrop.cs
+++ ForceDrop.cs
@@ -4,6 +4,7 @@ using Rocket.Core.Commands;
 using Rocket.Core.Plugins;
 using Rocket.Unturned.Chat;
 using Rocket.Unturned.Player;
+using SDG.Unturned;
 using System;
 using Logger = Rocket.Core.Logging.Logger;
 namespace ForceDrop
@@ -18,9 +19,7 @@ namespace ForceDrop
             {
                 Instance = this;
                 Logger.Log("[Plugin4U] ForceDrop loaded!");
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine("Make sure to visit our site at www.Plugin4U.cf");
-                Console.ResetColor();
+                Logger.Log("Make sure to visit our site at www.Plugin4U.cf", ConsoleColor.Magenta);
             }
             else {
                 Logger.Log("Configuration.Instance.Enabled == false");
@@ -37,6 +36,8 @@ namespace ForceDrop
                 return new TranslationList(){
                     {"drop_message","Successfully dropped {0} inventory!" },
                     {"drop_message_id","Successfully dropped {0} from {1} inventory!"},
+                    {"drop_message_public","Successfully dropped all players inventory!" },
+                    {"drop_message_id_public","Successfully dropped {0} from all players inventory!"},
                 };
             }
         }
@@ -46,7 +47,56 @@ namespace ForceDrop
         [RocketCommandAlias("Fd")]
         public void Execute(IRocketPlayer caller, params string[] command)
         {
-            if (command.Length == 1)
+            if(command[0].ToLower() == "all")
+            {
+                if(command.Length == 1)
+                {
+                    foreach(var steamplayer in Provider.clients)
+                    {
+                        UnturnedPlayer player = UnturnedPlayer.FromSteamPlayer(steamplayer);
+                        for (byte page = 0; page < 8; page++)
+                        {
+                            var count = player.Inventory.getItemCount(page);
+
+                            for (byte index = 0; index < count; index++)
+                            {
+                                foreach (var number in Configuration.Instance.WhiteListedFromDrop)
+                                {
+                                    if (player.Inventory.getItem(page, index).item.id == number)
+                                    { }
+                                    else
+                                    {
+                                        player.Inventory.askDropItem(player.CSteamID, page, player.Inventory.getItem(page, index).x, player.Inventory.getItem(page, index).y);
+                                    }
+                                }
+                            }
+                        }
+                        UnturnedChat.Say(caller, Translate("drop_message_public", ushort.Parse(command[1].ToString())));
+                    }
+                }
+                if(command.Length == 2)
+                {
+                    foreach(var steamplayer in Provider.clients)
+                    {
+                        UnturnedPlayer player = UnturnedPlayer.FromSteamPlayer(steamplayer);
+                        for (byte page = 0; page < 8; page++)
+                        {
+                            var count = player.Inventory.getItemCount(page);
+
+                            for (byte index = 0; index < count; index++)
+                            {
+                                if (player.Inventory.getItem(page, index).item.id == ushort.Parse(command[1].ToString()))
+                                {
+                                    player.Inventory.askDropItem(player.CSteamID, page, player.Inventory.getItem(page, index).x, player.Inventory.getItem(page, index).y);
+                                }
+                            }
+                        }
+                    }
+                    UnturnedChat.Say(caller, Translate("drop_message_id_public", ushort.Parse(command[1].ToString())));
+                }
+            }
+
+            if (command.Length == 1 && command[0].ToLower() != "all")
             {
                 UnturnedPlayer player = UnturnedPlayer.FromName(command[0]);
                 if (player != null)
@@ -71,7 +121,7 @@ namespace ForceDrop
                     UnturnedChat.Say(caller, Translate("drop_message", player.DisplayName));    
                 }
             }
-            if(command.Length == 2)
+            if(command.Length == 2 && command[0].ToLower() != "all")
             {
                 UnturnedPlayer player = UnturnedPlayer.FromName(command[0]);
                 if (player != null)
